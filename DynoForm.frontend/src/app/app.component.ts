958cc0f5dc4817e2e03b64968a5065e4edf59204
Niloo Mont <niloo.mon@gmail.com>
2025-01-04 15:42:36 +0330

    feat: Add landing

--- DynoForm.frontend/src/app/app.component.ts
+++ DynoForm.frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'DynoForm.frontend';
+  constructor(private router: Router) { }
+
+  navigateToNextPage(): void {
+    // this.router.navigate(['/next-page']);
+  }
 }
