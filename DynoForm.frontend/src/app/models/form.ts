733257572fcaa617c34cc1fbb40fae77f4b862f3
Niloo Mont <niloo.mon@gmail.com>
2025-02-02 19:07:41 +0330

    fix: remove description from form model

--- DynoForm.frontend/src/app/models/form.ts
+++ DynoForm.frontend/src/app/models/form.ts
@@ -1,12 +1,10 @@
 export class Form {
     Id: string;
     Title: string;
-    Description: string;
     JsonSchema: string;
-    constructor(Id = '00000000-0000-0000-0000-000000000000', Title = '', Description = '', JsonSchema = '') {
+    constructor(Id = '00000000-0000-0000-0000-000000000000', Title = '', JsonSchema = '') {
         this.Id = Id;
         this.Title = Title;
-        this.Description = Description;
         this.JsonSchema = JsonSchema;
     }
 }
